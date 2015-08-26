f3736c53085a0ccc7ec3a9560c06c893a6e9f3d2
y2k <itwisterlx@gmail.com>
2015-08-26 23:32:08 +0400

    private settings

---
+++ AppExpress/Models/Settings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AppExpress
+{
+    public class Settings
+    {
+        public static Settings Instance { get; } = new Settings();
+
+        Dictionary<string, string> data = new Dictionary<string, string>();
+
+        Settings()
+        {
+            using (var stream = GetType().Assembly.GetManifestResourceStream("AppExpress.private-settings.ini"))
+            {
+                var reader = new StreamReader(stream);
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var parts = line.Split('=').Select(s => s.Trim()).ToList();
+                    data[parts[0]] = parts[1];
+                }
+            }
+        }
+
+        public string Get(string key)
+        {
+            return data[key];
+        }
+    }
+}
